fd5efabf62aaa2901bb753f8ae65820259e556be
Franco D'Alessio <f.n.dalessio@gmail.com>
2025-08-08 16:46:54 -0300

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./app.css";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
